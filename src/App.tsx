fb9e4f322cf2961e56b0b7f8689c596ad1db858d
hrryndrsn <harryanderson9@gmail.com>
2018-12-15 21:18:57 +1100

    loading simple data into ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import creds from './key.json' 
-import axios from 'axios'
-import { threadId } from 'worker_threads';
-
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import creds from "./key.json";
+import axios from "axios";
+import BookList from "./components/bookList";
 
 interface Fields {
   bookTitle: string;
@@ -19,82 +18,71 @@ interface BookRecord {
   fields: Fields;
   createdTime: Date;
 }
-interface AppProps {
-  
-}
+interface AppProps {}
 interface AppState {
-  readingList: BookRecord[]
+  readingList: BookRecord[];
 }
 
 class App extends Component<AppProps, AppState> {
   constructor(props: AppProps) {
-    super(props)
+    super(props);
     this.state = {
       readingList: []
-    }
+    };
   }
 
   componentDidMount() {
-    this.fetchData(creds.key)
+    this.fetchData(creds.key);
   }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <BookList readingList={this.state.readingList} />
       </div>
     );
   }
 
   fetchData = (key: string) => {
-    let readingList: BookRecord[]
-    readingList = []
+    let readingList: BookRecord[];
+    readingList = [];
     //called in the then callback of the get promise
     let updateReadingList = (list: BookRecord[]) => {
-      this.setState({readingList: list})
-    }
-    axios.get(`https://api.airtable.com/v0/appz2wjE9XrYOZ7Lq/reading%20list?api_key=` + key)
-    .then(function (response) {
-      // handle success
-      // create a list of keys 
-      let kys = Object.keys(response.data.records);
-      let newRecord: BookRecord
+      this.setState({ readingList: list });
+    };
+    axios
+      .get(
+        `https://api.airtable.com/v0/appz2wjE9XrYOZ7Lq/reading%20list?api_key=` +
+          key
+      )
+      .then(function(response) {
+        // handle success
+        // create a list of keys
+        let kys = Object.keys(response.data.records);
+        let newRecord: BookRecord;
 
-      //interate through the arr of keys 
-      kys.forEach(element => {
-        let record = response.data.records[element]
-        newRecord = {
-          id: record.id,
-          fields: record.fields,
-          createdTime: record.createdTime
-        }
-        //append book records to the reading list
-        readingList.push(newRecord)
+        //interate through the arr of keys
+        kys.forEach(element => {
+          let record = response.data.records[element];
+          newRecord = {
+            id: record.id,
+            fields: record.fields,
+            createdTime: record.createdTime
+          };
+          //append book records to the reading list
+          readingList.push(newRecord);
+        });
+        //print out the new reading list
+        console.log(readingList);
+        updateReadingList(readingList);
+      })
+      .catch(function(error) {
+        // handle error
+        console.log(error);
+      })
+      .then(function() {
+        // always executed
       });
-      //print out the new reading list
-      console.log(readingList)
-      updateReadingList(readingList)
-    })
-    .catch(function (error) {
-      // handle error
-      console.log(error);
-    })
-    .then(function () {
-      // always executed
-    });
-  }
+  };
 }
 
 export default App;
